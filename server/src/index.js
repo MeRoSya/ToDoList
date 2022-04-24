bd9079aed58368ec86377f07d2acecfcd9c64675
Ivan Zhuginsky <Zhuga200117@gmail.com>
2022-04-24 19:47:21 +0300

    feat: :construction: Created stub for the server

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+
+app.post('/todos', (req, res) => {
+
+});
+
+app.delete('/todos', (req, res) => {
+
+});
+
+app.get('/todos', (req, res) => {
+
+});
+
+app.listen(process.env.PORT || 8080);
