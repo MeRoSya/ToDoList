2a380da854942ba0a9d53bd8fe9c4bb304d990ec
Жугинский Иван <zhuga200117@gmail.com>
2022-04-24 16:01:24 +0300

    feat: :sparkles: Created the simple prototype

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { v4 } from "uuid";
+import Adder from "./Adder";
+import ToDoList from "./ToDoList";
+
 export default function App() {
+  const [todos, setToDos] = useState([]);
+
+  const addToDo = (text) => {
+    setToDos([...todos, { id: v4(), content: text }])
+  }
+
+  const removeToDo = (id) =>
+    setTimeout(() => {
+      var newToDos = [...todos];
+      var indexToRemove = newToDos.findIndex(todo => todo.id === id);
+
+      if (indexToRemove > -1)
+        newToDos.splice(indexToRemove, 1);
+
+      setToDos(newToDos);
+    }, 250);
+
   return (
-    <></>
+    <>
+      <ToDoList toDos={todos} removeToDo={removeToDo} />
+      <Adder addToDo={addToDo} />
+    </>
   );
 }
